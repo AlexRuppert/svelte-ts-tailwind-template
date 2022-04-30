5b7fb463b16fafafb7579eea3062d8587ed57b81
AlexRuppert <alexander.ruppert@rwth-aachen.de>
2022-04-30 22:23:59 +0200

    initial

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  
+</script>
+
+
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+</style>
